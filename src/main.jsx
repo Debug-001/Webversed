32c56c43650211e285dbf9c4035939b3319dda1d
Om Bhatt <ombhatt1158@gmail.com>
2023-01-31 20:37:32 +0530

    Firebase integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import './assets/styles.scss'
 import '../node_modules/bootstrap'
 import './assets/js/script.min.js'
 
