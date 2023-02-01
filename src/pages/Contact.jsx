1d5b3ca68d64293689477ee81803aa1774fad565
Om Bhatt <ombhatt1158@gmail.com>
2023-02-01 19:56:22 +0530

    client side fixes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,8 @@ const Contact = () => {
   const [userData, setUserData] = useState({
     firstName: "",
     email: "",
-    number: "",
+    telephone:"",
+    message: ""
   });
 
   let name, value;
@@ -19,11 +20,11 @@ const Contact = () => {
 
   const submitData = async (event) => {
     event.preventDefault();
-    const { firstName, email, number } = userData
-    if (firstName && email && number) {
+    const { firstName, email, telephone, message } = userData
+    if (firstName && email &&  telephone && message) {
 
       const res = fetch(
-        'https://webversed-430f8-default-rtdb.firebaseio.com/userData.json',
+        'https://webversedcontact-default-rtdb.firebaseio.com/contactData.json',
         {
           method: "POST",
           Headers: {
@@ -32,7 +33,8 @@ const Contact = () => {
           body: JSON.stringify({
             firstName,
             email,
-            number
+            telephone,
+            message
           })
         }
       )
@@ -40,9 +42,10 @@ const Contact = () => {
         setUserData({
           firstName: "",
           email: "",
-          number: "",
+          telephone:"",
+          message: ""
         })
-        alert("Data stored")
+        alert("Your response has been saved. Expect a reply soon!")
       } else {
         alert("please fill the data")
       }
@@ -104,7 +107,8 @@ const Contact = () => {
           </div>
         </div>
       </nav>
-      {/* End: Navbar Centered Links */}
+     
+
       {/* Start: Contact Details */}
       <section id="contact" className="py-5">
         <div className="container">
@@ -143,15 +147,24 @@ const Contact = () => {
                       onChange={postUserData}
                     />
                   </div>
+                  <div className="mb-3">
+                    <input
+                      className="form-control"
+                      type="tel"
+                      name="telephone"
+                      placeholder="Contact Number"
+                      value={userData.telephone}
+                      onChange={postUserData}
+                    />
+                  </div>
                   {/* End: Error Example */}
                   <div className="mb-3">
                     <textarea
                       className="form-control"
-                      id="number"
-                      name="number"
-                      rows={1}
-                      placeholder="Enter your Whatsapp number"
-                      value={userData.number}
+                      name="message"
+                      rows={4}
+                      placeholder="Enter your query"
+                      value={userData.message}
                       onChange={postUserData}
                     />
                   </div>
@@ -169,6 +182,7 @@ const Contact = () => {
               <div className="d-flex flex-wrap flex-md-column justify-content-md-start align-items-md-start h-100">
                 <div className="d-flex align-items-center p-3">
                   <div className="bs-icon-md bs-icon-circle bs-icon-primary shadow d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon bs-icon-md">
+                    <a href="https:/wa.me/+918726879756">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="1em"
@@ -176,17 +190,21 @@ const Contact = () => {
                       fill="currentColor"
                       viewBox="0 0 16 16"
                       className="bi bi-whatsapp"
-                    >
+                      >
                       <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"></path>
                     </svg>
+                      </a>
                   </div>
                   <div className="px-2">
                     <h6 className="fw-bold mb-0">Whatsapp</h6>
-                    <p className="text-muted mb-0">Click here!</p>
+                    <a href="https:/wa.me/+918726879756">
+                    <p id='footer_text' className="text-muted mb-0">Click here!</p>
+                    </a>
                   </div>
                 </div>
                 <div className="d-flex align-items-center p-3">
                   <div className="bs-icon-md bs-icon-circle bs-icon-primary shadow d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon bs-icon-md">
+                    <a href="mailto:syedmohdhaider2@gmail.com">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="1em"
@@ -194,22 +212,26 @@ const Contact = () => {
                       fill="currentColor"
                       viewBox="0 0 16 16"
                       className="bi bi-envelope"
-                    >
+                      >
                       <path
                         fillRule="evenodd"
                         d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"
-                      ></path>
+                        ></path>
                     </svg>
+                        </a>
                   </div>
                   <div className="px-2">
                     <h6 className="fw-bold mb-0">Email</h6>
-                    <p className="text-muted mb-0">
+                    <a href="mailto:syedmohdhaider2@gmail.com">
+                    <p id='footer_text' className="text-muted mb-0">
                       webversed021@gmail.com (Usually takes 2-4 days to reply)
                     </p>
+                    </a>
                   </div>
                 </div>
                 <div className="d-flex align-items-center p-3">
                   <div className="bs-icon-md bs-icon-circle bs-icon-primary shadow d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block bs-icon bs-icon-md">
+                    <a href="https://t.me/web_versed_hotline">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="1em"
@@ -217,15 +239,17 @@ const Contact = () => {
                       fill="currentColor"
                       viewBox="0 0 16 16"
                       className="bi bi-telegram"
-                    >
+                      >
                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
                     </svg>
+                      </a>
                   </div>
                   <div className="px-2">
                     <h6 className="fw-bold mb-0">Telegram</h6>
-                    <p className="text-muted mb-0">
+                    <a href="https://t.me/web_versed_hotline"><p id='footer_text' className="text-muted mb-0">
                       &nbsp;Join our TG for instant reply (Replies within 2-5 mins)
                     </p>
+                    </a>
                   </div>
                 </div>
               </div>
@@ -233,8 +257,10 @@ const Contact = () => {
           </div>
         </div>
       </section>
-      {/* End: Contact Details */}
-      {/* Start: Footer Multi Column */}
+
+
+
+      {/* Start: Footer */}
       <footer className="bg-dark">
         <div className="container py-4 py-lg-5">
           <div className="row justify-content-center">
@@ -301,16 +327,18 @@ const Contact = () => {
             <p className="mb-0">Copyright © 2023 WebVersed</p>
             <ul className="list-inline mb-0">
               <li className="list-inline-item">
-                <svg
+               <a href="https://t.me/web_versed_hotline">
+                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="1em"
                   height="1em"
                   fill="currentColor"
                   viewBox="0 0 16 16"
                   className="bi bi-telegram"
-                >
+                  >
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
                 </svg>
+                  </a>
               </li>
               <li className="list-inline-item">
                 <svg
@@ -340,7 +368,6 @@ const Contact = () => {
           </div>
         </div>
       </footer>
-      {/* End: Footer Multi Column */}
     </>
 
   )
