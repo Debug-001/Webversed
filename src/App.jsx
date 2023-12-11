a7f51c804456eb4fba8609956d6775e4f4e49d27
Om Sharma <ombhatt1158@gmail.com>
2023-12-11 16:41:58 +0530

    init rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
 import React from "react";
-import { useState } from 'react'
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router";
-import Prem from "./pages/Prem";
-import Pay_basic from "./pages/Pay_basic";
-import Pay_pre from "./pages/Pay_pre";
-import Basic from "./pages/Basic";
-import Pricing from "./pages/Pricing";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import About from "./pages/About"
-import Card from "./pages/Card";
-import Card2 from "./pages/Card2";
-import Card3 from "./pages/Card3";
-import Card4 from "./pages/Card4";
-import Card5 from "./pages/Card5";
-import CardInfo from "./pages/CardInfo"
+import Home from "./pages/Home";
 
 function App() {
-	const [infoData, setInfoData] = useState(CardInfo);
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/prem" element={<Prem />} />
-				<Route path="/pay_basic" element={<Pay_basic />} />
-				<Route path="/pay_pre" element={<Pay_pre />} />
-				<Route path="/basic" element={<Basic />} />
-				<Route path="/pricing" element={<Pricing />} />
-				<Route path="/services" element={<Services />} />
-				<Route path="/card" element={<Card infoData={infoData} />} />
-				<Route path="/card2" element={<Card2 />} />
-				<Route path="/card3" element={<Card3 />} />
-				<Route path="/card4" element={<Card4 />} />
-				<Route path="/card5" element={<Card5 />} />
 			</Routes>
 		</>
 	);
